e31f125e0510a2b16623c851c75a4eada80ecda7
Dylan Harper <39470230+dylan-harper@users.noreply.github.com>
2021-10-19 10:01:57 -0700

    Update cell.rb
    Remove test edits so we can create pull request

--- lib/cell.rb
+++ lib/cell.rb
@@ -13,25 +13,4 @@ class Cell
   def place_ship(cruiser)
     @ship = cruiser
   end
-  #adding some ideas for what these methods could be
-  def fired_upon?
-    if @ship.health == @ship.length #unsure about this method
-      false
-    else
-      true
-    end
-  end
-  def fire_upon
-   # if @ship != nil
-    #  puts "."
-   # else
-      @ship.hit
-  end
-  def render
-    if cell.fired_upon? == false
-      puts "."
-    elsif cell.fired_upon? == true || @ship == nil
-      puts "M"
-    elsif ...
-  end
 end
