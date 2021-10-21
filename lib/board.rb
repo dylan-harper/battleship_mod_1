f33bfa48ab75e4e1d4eab823c8ecbede3704d9ba
Dylan Harper <dylanharper4@icloud.com>
2021-10-21 12:31:39 -0700

    Add #validating_coordinates method and test

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,9 @@ require './lib/cell'
 
 class Board
 
+
   def initialize
+    #can we make the below more dynamic?
     @coords = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4']
 
   end
@@ -17,5 +19,9 @@ class Board
     end
 
     new_hash
+  end
+
+  def valid_coordinate?(coordinate)
+    @coords.include?(coordinate)
   end
 end
