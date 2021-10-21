7cade8fce1ea0d2fca2d54030e05b11b1e7dc9cb
Dylan Harper <dylanharper4@icloud.com>
2021-10-20 22:07:53 -0700

    more work on iteration 2

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,21 @@
+require './lib/cell'
+
 class Board
+
+  def initialize(cells = nil)
+    @cells = cells
+  end
+
+  def cells
+    @cells.map do |cell|
+      board = Board.new
+      cell.new(coordinate)
+  end
+
+  end
+
+  def cells_instances
+    @cell_instances ||= cells
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,35 @@
 require 'rspec'
 require './lib/board'
 
-Spec.describe Cell do
+RSpec.describe Board do
   before(:each) do
     @board = Board.new
-    
   end
+
+  it 'exists' do
+    expect(@board).to be_a(Board)
+  end
+
+  it '#cells contains all cells in a hash' do
+    expect(@board.cells).to eq(
+      {
+      "A1" => an_instance_of(Cell),
+      "A2" => an_instance_of(Cell),
+      "A3" => an_instance_of(Cell),
+      "A4" => an_instance_of(Cell),
+      "B1" => an_instance_of(Cell),
+      "B2" => an_instance_of(Cell),
+      "B3" => an_instance_of(Cell),
+      "B4" => an_instance_of(Cell),
+      "C1" => an_instance_of(Cell),
+      "C2" => an_instance_of(Cell),
+      "C3" => an_instance_of(Cell),
+      "C4" => an_instance_of(Cell),
+      "D1" => an_instance_of(Cell),
+      "D2" => an_instance_of(Cell),
+      "D3" => an_instance_of(Cell),
+      "D4" => an_instance_of(Cell)
+      }
+    )
+  end
+end
