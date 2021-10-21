6c2f17d578e85fea1b27a4b637640129d9bf0420
Jacob Jenner <jennerjacob@gmail.com>
2021-10-20 23:01:14 -0600

    Add more code to board.rb and board_spec.rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,23 @@
+require './lib/cell'
+
 class Board
+
+def initialize
+  @cells = %w[A1 A2 A3 A4 B1 B2 B3 B4 C1 C2 C3 C4 D1 D2 D3 D4]
+
+end
+
+def cells
+  new_hash = {}
+  @cells.each do |cell|
+    new_hash|1..4 && A..D| = Cell.new
+ end
+new_hash
+
+  # for c in @cells do
+  #   new_hash << Cell.new(coordinate)
+  # end
+  # new_hash
+end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,25 @@
 require 'rspec'
 require './lib/board'
 
-Spec.describe Cell do
+RSpec.describe Board do
   before(:each) do
     @board = Board.new
-    
+    coordinates = %w[A1 A2 A3 A4 B1 B2 B3 B4 C1 C2 C3 C4 D1 D2 D3 D4]
+    # cells = coordinates.map do |coordinate|
+    #   Cell.new(coordinate)
+    # end
   end
+
+  it 'exists' do
+    expect(@board).to be_a(Board)
+  end
+
+  it 'board has cells' do
+    expect(@board.cells).to be_a(Hash)
+    @board.cells.each do |coordinate, cell|
+      expect(cell.coordinate).to eq(coordinate)
+    end
+    require 'pry';binding.pry
+    expect(@board.cells.keys.length).to eq(16)
+  end
+end
