997087322256ae3a5fab0ae24d04d9c62a02dae4
Jacob Jenner <jennerjacob@gmail.com>
2021-10-21 20:37:00 -0600

    Edit cells method

--- lib/board.rb
+++ lib/board.rb
@@ -6,27 +6,28 @@ class Board
 
   def initialize
     #can we make the below more dynamic?
-    @coords = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4']
+    @coordinates = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4']
 
   end
 
   def cells
 
-    new_hash = {}
-    @coords.each do |i|
+    board_cells = {}
+    @coordinates.each do |coordinate|
       #makes i equal key and new cell instance equal value
-      new_hash.store(i, cell = Cell.new(i))
+      board_cells[coordinate] = Cell.new(coordinate)
+      # board_cells.store(coordinate, cell = Cell.new(i))
       #stores new keyvalue in hash accumulator
     end
 
-    new_hash
+    board_cells
   end
 
   def valid_coordinate?(coordinate)
-    @coords.include?(coordinate)
+    @coordinates.include?(coordinate)
   end
 
-  def valid_placement?(ship, coordinate)
-    ship.length == coordinate.count
+  def valid_placement?(ship, coordinates)
+    (ship.length == coordinates.count) # && consecutive_coordinates
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -21,18 +21,22 @@ RSpec.describe Board do
   it '#valid_coordinate? tells us if the coordinate is on the board or not' do
 
 
-  expect(@board.valid_coordinate?('A1')).to be(true)
-  expect(@board.valid_coordinate?('D4')).to be(true)
-  expect(@board.valid_coordinate?('A5')).to be(false)
-  expect(@board.valid_coordinate?('E1')).to be(false)
-  # require 'pry';binding.pry
-  expect(@board.valid_coordinate?('A22')).to be(false)
+    expect(@board.valid_coordinate?('A1')).to be(true)
+    expect(@board.valid_coordinate?('D4')).to be(true)
+    expect(@board.valid_coordinate?('A5')).to be(false)
+    expect(@board.valid_coordinate?('E1')).to be(false)
+    expect(@board.valid_coordinate?('A22')).to be(false)
   end
 
-  it '#valid_placement? tells us if the ship length equals number of coords' do
-  submarine = Ship.new("Submarine", 2)
-  cruiser = Ship.new("Cruiser", 3)
-  expect(@board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be(false)
-  expect(@board.valid_placement?(cruiser, ["A1", "A2"])).to be(false)
-    end
+  it '#valid_placement? ship and coordinates length not equal' do
+    submarine = Ship.new("Submarine", 2)
+    cruiser = Ship.new("Cruiser", 3)
+    expect(@board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be(false)
+    require 'pry';binding.pry
+    expect(@board.valid_placement?(cruiser, ["A1", "A2"])).to be(false)
   end
+
+  it '#valid_placement ship and coordinates length equal' do
+
+  end
+end
