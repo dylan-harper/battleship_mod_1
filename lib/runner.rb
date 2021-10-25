fd3809ffbea53f66d13ef997b50efd276f01b6dc
Dylan Harper <dylanharper4@icloud.com>
2021-10-24 21:15:40 -0700

    Get render method to print board

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,30 +1,37 @@
-require './lib/ship'
-require './lib/board'
-require './lib/cell'
+require_relative './ship'
+require_relative './board'
+require_relative './cell'
 
-cell_1 = @board.cells['A1']
-cell_2 = @board.cells['A2']
-cell_3 = @board.cells['A3']
-cell_4 = @board.cells['A4']
-cell_5 = @board.cells['B1']
-cell_6 = @board.cells['B2']
-cell_7 = @board.cells['B3']
-cell_8 = @board.cells['B4']
-cell_9 = @board.cells['C1']
-cell_10 = @board.cells['C2']
-cell_11 = @board.cells['C3']
-cell_12 = @board.cells['C4']
-cell_13 = @board.cells['D1']
-cell_14 = @board.cells['D2']
-cell_15 = @board.cells['D3']
-cell_16 = @board.cells['D4']
+board = Board.new
+board.cells
 
-
-puts o = " " + "1" + "2" + "3" + "4" + "/n"
-puts a = 'A' + cell_1.render + cell_2.render + cell_3.render + cell_4.render + '/n'
-puts b = 'B' + cell_5.render + cell_6.render + cell_7.render + cell_8.render + '/n'
-puts c = 'C' + cell_9.render + cell_10.render + cell_11.render + cell_12.render '/n'
-puts d = 'D' + cell_13.render + cell_14.render + cell_15.render + cell_16.render + '/n' 
+cell_1 = board.board_cells['A1']
+cell_2 = board.board_cells['A2']
+cell_3 = board.board_cells['A3']
+cell_4 = board.board_cells['A4']
+cell_5 = board.board_cells['B1']
+cell_6 = board.board_cells['B2']
+cell_7 = board.board_cells['B3']
+cell_8 = board.board_cells['B4']
+cell_9 = board.board_cells['C1']
+cell_10 = board.board_cells['C2']
+cell_11 = board.board_cells['C3']
+cell_12 = board.board_cells['C4']
+cell_13 = board.board_cells['D1']
+cell_14 = board.board_cells['D2']
+cell_15 = board.board_cells['D3']
+cell_16 = board.board_cells['D4']
 
 
-puts grid
+
+board = [['A' + cell_1.render + cell_2.render + cell_3.render + cell_4.render + '/n']]
+puts board
+
+# puts o = " " + "1" + "2" + "3" + "4" + "/n"
+# puts a = 'A #{cell_1.render}  #{cell_2.render}  #{cell_3.render} #{cell_4.render} /n'
+# puts b = 'B #{cell_5.render}  #{cell_6.render}  #{cell_7.render} #{cell_8.render} /n'
+# puts c = 'C #{cell_9.render}  #{cell_10.render}  #{cell_11.render} #{cell_12.render} /n'
+# puts d = 'D #{cell_13.render}  #{cell_14.render}  #{cell_15.render} #{cell_16.render} /n'
+#
+#
+# puts grid
