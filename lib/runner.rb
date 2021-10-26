d1608bc7ba55f5b1c019cd52197ca3acec9846e0
Jacob Jenner <jennerjacob@gmail.com>
2021-10-26 12:35:58 -0600

    Add Main Menu

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,37 +1,48 @@
-require_relative './ship'
-require_relative './board'
-require_relative './cell'
-
-board = Board.new
-board.cells
-
-cell_1 = board.board_cells['A1']
-cell_2 = board.board_cells['A2']
-cell_3 = board.board_cells['A3']
-cell_4 = board.board_cells['A4']
-cell_5 = board.board_cells['B1']
-cell_6 = board.board_cells['B2']
-cell_7 = board.board_cells['B3']
-cell_8 = board.board_cells['B4']
-cell_9 = board.board_cells['C1']
-cell_10 = board.board_cells['C2']
-cell_11 = board.board_cells['C3']
-cell_12 = board.board_cells['C4']
-cell_13 = board.board_cells['D1']
-cell_14 = board.board_cells['D2']
-cell_15 = board.board_cells['D3']
-cell_16 = board.board_cells['D4']
 
 
 
-board = [['A' + cell_1.render + cell_2.render + cell_3.render + cell_4.render + '/n']]
-puts board
+puts "Welcome to BATTLESHIP"
+puts "Enter p to play. Enter q to quit"
 
-# puts o = " " + "1" + "2" + "3" + "4" + "/n"
-# puts a = 'A #{cell_1.render}  #{cell_2.render}  #{cell_3.render} #{cell_4.render} /n'
-# puts b = 'B #{cell_5.render}  #{cell_6.render}  #{cell_7.render} #{cell_8.render} /n'
-# puts c = 'C #{cell_9.render}  #{cell_10.render}  #{cell_11.render} #{cell_12.render} /n'
-# puts d = 'D #{cell_13.render}  #{cell_14.render}  #{cell_15.render} #{cell_16.render} /n'
+user_input = gets.chomp
+
+
+
+
+# require_relative './ship'
+# require_relative './board'
+# require_relative './cell'
+#
+# board = Board.new
+# board.cells
+#
+# cell_1 = board.board_cells['A1']
+# cell_2 = board.board_cells['A2']
+# cell_3 = board.board_cells['A3']
+# cell_4 = board.board_cells['A4']
+# cell_5 = board.board_cells['B1']
+# cell_6 = board.board_cells['B2']
+# cell_7 = board.board_cells['B3']
+# cell_8 = board.board_cells['B4']
+# cell_9 = board.board_cells['C1']
+# cell_10 = board.board_cells['C2']
+# cell_11 = board.board_cells['C3']
+# cell_12 = board.board_cells['C4']
+# cell_13 = board.board_cells['D1']
+# cell_14 = board.board_cells['D2']
+# cell_15 = board.board_cells['D3']
+# cell_16 = board.board_cells['D4']
 #
 #
-# puts grid
+#
+# board = [['A' + cell_1.render + cell_2.render + cell_3.render + cell_4.render + '/n']]
+# puts board
+#
+# # puts o = " " + "1" + "2" + "3" + "4" + "/n"
+# # puts a = 'A #{cell_1.render}  #{cell_2.render}  #{cell_3.render} #{cell_4.render} /n'
+# # puts b = 'B #{cell_5.render}  #{cell_6.render}  #{cell_7.render} #{cell_8.render} /n'
+# # puts c = 'C #{cell_9.render}  #{cell_10.render}  #{cell_11.render} #{cell_12.render} /n'
+# # puts d = 'D #{cell_13.render}  #{cell_14.render}  #{cell_15.render} #{cell_16.render} /n'
+# #
+# #
+# # puts grid
