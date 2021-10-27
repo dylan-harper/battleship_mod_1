e5de3a3cdf33fe6b5104730d541652e09a74341b
Dylan Harper <39470230+dylan-harper@users.noreply.github.com>
2021-10-27 10:13:02 -0700

    Update runner.rb

--- lib/runner.rb
+++ lib/runner.rb
@@ -59,59 +59,6 @@ def game_initiate
     @board.place(player_cruiser, cruiser_coords)
   end
 
-
-#   puts "First cruiser coordinate:"
-#   cruiser_coord_1 = gets.chomp.to_s
-#
-#   puts "Second cruiser coordinate:"
-#   cruiser_coord_2 = gets.chomp.to_s
-#   puts "Third cruiser coordinate:"
-#   cruiser_coord_3 = gets.chomp
-#
-#   if @board.valid_coordinate?(cruiser_coord_1)
-#     cruiser_coords << cruiser_coord_1
-#   elsif !@board.valid_coordinate?(cruiser_coord_1)
-#     while !@board.valid_coordinate?(cruiser_coord_1) do
-#       puts "Incorrect coordinate. Try again."
-#       cruiser_coord_1 = gets.chomp.to_s
-#     end
-#     cruiser_coords << cruiser_coord_1
-#   end
-#
-# require "pry"; binding.pry
-#
-#   puts "Second coordinate:"
-#   cruiser_coord_2 = gets.chomp
-#
-#   if @board.valid_coordinate?(cruiser_coord_2)
-#     cruiser_coords << cruiser_coord_2
-#   elsif !@board.valid_coordinate?(cruiser_coord_2)
-#     while !@board.valid_coordinate?(cruiser_coord_2) do
-#       puts "Incorrect coordinate. Try again."
-#       cruiser_coord_2 = gets.chomp
-#     end
-#     cruiser_coords << cruiser_coord_2
-#   end
-#
-#   require "pry"; binding.pry
-
-  # cruiser_coords << cruiser_coord_2
-  # puts "Third coordinate:"
-  # cruiser_coord_3 = gets.chomp
-  # cruiser_coords<< cruiser_coord_3
-  #
-  # puts "Enter the squares for the Submarine (2 spaces:)"
-  # sub_coords = []
-  #
-  #
-  # sub_coords << sub_coord_1
-  # puts "Second coordinate:"
-  # sub_coord_2 = gets.chomp
-  # sub_coords << sub_coord_2
-  #
-  #
-  # p cruiser_coords
-  # p sub_coords
 end
 
 
